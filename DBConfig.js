a907b57e535f94608a110e5ccc20e6a7bda17ef2
Eleftherios Adalakis <adalakis.el@gmail.com>
2024-01-18 15:27:26 +0100

    removed query

--- DBConfig.js
+++ DBConfig.js
@@ -28,28 +28,3 @@ function connectDB() {
 }
 
 connectDB();
-
-// const uri =
-//   "mongodb+srv://admin:J5qD5gCaU4HGc8T@cluster0.bu2drdk.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a new MongoClient
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await mongoose.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
